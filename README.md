c53fadf2085567cf278b73e66851733e65ef65b6
zoubin2019102976 <61451691+zoubin2019102976@users.noreply.github.com>
2023-02-26 22:35:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qdsi7z-kn5bwv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qdsi7z-by1xot)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const routerParams = this.route.snapshot.paramMap;
+    const productIdFromRouter = Number(routerParams.get('productId'));
+  }
+}
