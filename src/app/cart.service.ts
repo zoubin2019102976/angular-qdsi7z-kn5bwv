ced8d681f4a7f538b1c58b51a07781a2c9880356
zoubin2019102976 <61451691+zoubin2019102976@users.noreply.github.com>
2023-02-26 23:22:10 +0800

    数据管理-3.使用httpClient获取配送费用相关信息

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   items: Product[] = [];
 
   addToCar(product: Product) {
@@ -20,5 +21,9 @@ export class CartService {
   clearCar() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
